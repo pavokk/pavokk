e5666a75727135495add46b39f46d25001f57662
pavokk <pavokk@gmail.com>
2022-03-24 14:54:31 +0100

    Add files via upload

---
+++ id_to_name.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+# Replaces a column with ID's in 1 file with a mapping from another file. The mapping-file can only have 2 columns.
+
+
+def id_to_name(source, target, t_column):
+    df_brands = pd.read_csv(source)
+    df_products = pd.read_csv(target)
+
+    brand_map = dict(df_brands.values)
+    increment = 0
+
+    for value in df_products[t_column]:
+        df_products[t_column][increment] = brand_map[value]
+        increment += 1
+
+    df_products.to_csv('replaced-list.csv')
+
+
+id_to_name('mock-brands.csv', 'mock-products.csv', 'Producer')
