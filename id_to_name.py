cade97e2d675c4a524b147443e0543d568588a26
pavokk <pavokk@gmail.com>
2022-04-04 12:18:08 +0200

    Support for more values
    Added support for files with values that is not included in mapping file, skipping these.

--- id_to_name.py
+++ id_to_name.py
@@ -4,15 +4,19 @@ pd.options.mode.chained_assignment = None # Ignoring a pandas error
 # Replaces a column with ID's in 1 file with a mapping from another file. The mapping-file can only have 2 columns.
 
 
-def id_to_name(source, target, t_column):
+def id_to_name(source, target, t_column_id, t_column_model):
     print('Start!')
     dfs = pd.read_csv(source)
     dft = pd.read_csv(target)
     brand_map = dict(dfs.values)
     increment = 0
 
-    for value in dft[t_column]:
-        dft[t_column][increment] = brand_map[value]
+    for value in dft[t_column_id]:
+        if value in brand_map.keys():
+            dft[t_column_model][increment] = brand_map[value]
+        else:
+            pass
+        
         increment += 1
 
     dft.to_csv('replaced-list.csv')
